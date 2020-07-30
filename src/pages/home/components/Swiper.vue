dacd18122bf1bf6b40ee8857346eca0b661e4c52
peirong-js <56344165+peirong-js@users.noreply.github.com>
2020-07-30 11:00:21 +0800

    轮播图插件vue-awesome-swiper
    快速构建轮播图插件vue-awesome-swiper ：
    npm install vue-awesome-swiper@2.6.7 --save

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+		name:'HomeSwiper',
+		data () {
+			return {
+				swiperOption: {
+        	pagination: '.swiper-pagination',
+        	loop: true
+      	},
+      	swiperList:[{
+      		id:'0001',
+      		imgUrl:'http://img1.qunarzz.com/sight/p0/1905/af/af4f95f579ee43b1a3.water.jpg_600x330_164b10ec.jpg'
+      	},{
+      		id:'0002',
+      		imgUrl:'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_600x330_9fee26e9.jpg'
+      	}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 53%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
